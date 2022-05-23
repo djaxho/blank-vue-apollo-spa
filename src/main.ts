443138f6a3d7523aa591de71ab4681e116acc762
Danny Jackson <djaxho@gmail.com>
2022-05-23 21:08:25 +0200

    feat: vue apollo

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,35 @@
-import { createApp } from "vue";
+import { createApp, provide, h } from 'vue'
 import { createPinia } from "pinia";
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+
 
 import App from "./App.vue";
 import router from "./router";
 import "./index.css";
 
-const app = createApp(App);
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: 'http://localhost/graphql',
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+})
+
+const app = createApp({
+  setup () {
+    provide(DefaultApolloClient, apolloClient)
+  },
+
+  render: () => h(App),
+})
 
 app.use(createPinia());
 app.use(router);
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,26 @@
-<script setup lang="ts">
-import TheWelcome from "@/components/TheWelcome.vue";
-</script>
 
 <template>
   <main>
     <TheWelcome />
+    {{ loading ? 'loading still' : 'done' }}
+    {{ error }}
+    {{ result }}
   </main>
 </template>
+
+<script setup lang="ts">
+import TheWelcome from "@/components/TheWelcome.vue";
+import { useQuery } from '@vue/apollo-composable'
+import gql from 'graphql-tag'
+
+const { result, loading, error } = useQuery(gql`
+      query UserQuery {
+        user(id: 1) {
+          id
+          email
+          created_at
+          updated_at
+        }
+      }
+    `)
+</script>
